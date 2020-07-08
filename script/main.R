7e2463a951bef103bab145476e9c78317d350e72
Deus Thindwa <deus.thindwa@gmail.com>
2020-07-08 13:35:52 +0100

    code

--- script/main.R
+++ script/main.R
@@ -12,7 +12,7 @@ data.frame(age = 55:100,
            Pop = 910000 - cumsum(rep(19,46))*1000,  # pup UK like
            IPD.inc = 1.1^(1:46)) %>%                # IPD cases made up
   mutate(IPD.cases = Pop*IPD.inc) %>%
-  mutate(VE = c(rep(0,Vac.age-55),Ve*2^(-1/half*1:(46+55-Vac.age)))) 
+  mutate(VE = c(rep(0,Vac.age-55),Ve*2^(-1/half*1:(46+55-Vac.age)))) %>%
   mutate(Impact = VE * IPD.cases) -> dt
 
 dt$Impact %>% sum %>% print
