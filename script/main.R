09e7cf17b2f35400a6c1385fdd386c2fad0ff1f2
Deus Thindwa <deus.thindwa@gmail.com>
2020-07-14 08:21:53 +0100

    code

--- script/main.R
+++ script/main.R
@@ -1,25 +1,18 @@
-#load the require package
-require(tidyverse)
-require(curl)
-require(Hmisc)
+#load the require packages
+require(pacman)
+pacman::p_load(char = c("tidyverse", "curl", "Hmisc"))
 
 #load the IPD cases
 IPD <- read.csv(curl("https://raw.githubusercontent.com/deusthindwa/optimal.age.pneumovacc.adults/master/data/EW_ipd_incid.csv")) 
 POP <- read.csv(curl("https://raw.githubusercontent.com/deusthindwa/optimal.age.pneumovacc.adults/master/data/EW_total_pop.csv")) 
 
-#add age increment
-IPD$agey <- if_else(IPD$agegroup=="55-59",55,
-                      if_else(IPD$agegroup=="60-64",60,
-                              if_else(IPD$agegroup=="65-69",65,
-                                      if_else(IPD$agegroup=="70-74",70,
-                                              if_else(IPD$agegroup=="75-79",75,
-                                                      if_else(IPD$agegroup=="80-84",80,85))))))
+IPD <- mutate(IPD, agey = readr::parse_number(substr(agegroup,1,2)))
 
 #estimate the rest of parameters using a simple linear model
 theta0 <- min(IPD$incidence,na.rm=TRUE)*0.5  
 model0 <- lm(log(incidence-theta0) ~ agey, data=IPD)  
 alpha0 <- exp(coef(model0)[1])
-beta0 <- coef(model0)[2] 
+beta0  <- coef(model0)[2] 
 
 #Initial parameter values
 start <- list(alpha=alpha0, beta=beta0, theta=theta0)
@@ -40,30 +33,29 @@ ggplot() +
 
 #generate IPD cases from total pop and IPD incidence annually
 Cases <- data_frame(agey=seq(from=55,to=90,by=1)) %>% mutate(all.incid=predict(model.all,list(agey=agey))) %>%
-mutate(pcv13.incid=predict(model.pcv13,list(agey=agey))) %>% mutate(ppv23.incid=predict(model.ppv23,list(agey=agey)))
+  mutate(pcv13.incid=predict(model.pcv13,list(agey=agey))) %>% mutate(ppv23.incid=predict(model.ppv23,list(agey=agey)))
 Cases <- merge(Cases,POP)
 Cases$all.cases <- Cases$all.incid*Cases$ntotal
 Cases$pcv13.cases <- Cases$pcv13.incid*Cases$ntotal
 Cases$ppv23.cases <- Cases$ppv23.incid*Cases$ntotal
 
-#estimate baseline age-dependent vaccine efficacy against PPV23 serotypes
-Cases$Ve <- if_else(Cases$agey>=55 & Cases$agey<65, 0.17,
-                    if_else(Cases$agey>=65 & Cases$agey<=75, 0.25, 0.35))
-
-#estimate vaccine impact against PPV23 serotypes
-#Ve=0.5
+#estimate vaccine impact against all IPD serotypes
+Ve=0.5
 half=5
 Vac.age=55
 Cases <- Cases %>% mutate(VE=c(rep(0,Vac.age-55),Ve*2^(-1/half*1:(36+55-Vac.age)))) %>% mutate(Impact=VE*all.cases)
-plot(Cases$agey, Cases$Impact,type="l",col="blue", ylim=c(0,1e+07),xlim=c(Vac.age,90))
+plot(Cases$agey, Cases$Impact,type="l",col="blue", ylim=c(0,3e+06),xlim=c(Vac.age,90))
+points(Cases$Impact %>% sum)
 
-for(j in c(60,65,70,75,80,85)){
+for(i in c(0.5)){
+  for(j in c(60,65,70,75,80,85)){
     Ve=i
     half=5
     Vac.age=j
     Cases <- Cases %>% mutate(VE=c(rep(0,Vac.age-55),Ve*2^(-1/half*1:(36+55-Vac.age)))) %>% mutate(Impact=VE*all.cases)
-    lines(Cases$agey, Cases$Impact,col=topo.colors(j,alpha=1),xlim=c(Vac.age,87), lwd=2)
+    lines(Cases$agey, Cases$Impact,col=topo.colors(j,alpha=1),xlim=c(Vac.age,90), lwd=2)
   }
+}
 
 Ve =0.2 #first year Ve against VT IPD (currently age independent)
 half = 5 #half life of Vein years assuming exponential decay
@@ -77,7 +69,6 @@ Cases <- Cases %>%
 Cases$Impact %>% sum %>% print
 plot(Cases$agey, Cases$Impact)
 lines(Cases$agey, Cases$Impact)
-
 
 
 
