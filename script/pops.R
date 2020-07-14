33d1795036b1dfca104a5bffb01a7f0d4c00a659
Deus Thindwa <deus.thindwa@gmail.com>
2020-07-14 11:28:24 +0100

    code

--- script/pops.R
+++ script/pops.R
@@ -3,13 +3,15 @@
 
 countries <- c("England/Wales"="blue", "Malawi"="red")
 
-ggplot() +
-  geom_line(aes(x=pop.ew$agey, y=pop.ew$ntotal*100/sum(pop.ew$ntotal), color="England/Wales")) + 
-  geom_line(aes(x=pop.mw$agey, y=pop.mw$ntotal*100/sum(pop.mw$ntotal), color="Malawi")) + 
+A <- ggplot() +
+  geom_line(aes(x=pop.ew$agey, y=pop.ew$ntotal*100/sum(pop.ew$ntotal), color="England/Wales"), size=0.8) + 
+  geom_line(aes(x=pop.mw$agey, y=pop.mw$ntotal*100/sum(pop.mw$ntotal), color="Malawi"), size=0.8) + 
   scale_x_continuous(breaks = seq(55,90,5)) + 
   scale_y_continuous(breaks = seq(0,6.5,0.5)) + 
   theme_bw() + 
   labs(title="", x="Age (years)", y="Population %", color="Countries") +
   scale_color_manual(values=countries) +
   theme(axis.text.x=element_text(face="bold", size=10, color="black"), axis.text.y=element_text(face="bold", size=10, color="black"))
-  
+
+print(ggarrange(A,ncol=1))
+remove(A,countries)
