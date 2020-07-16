b565a2862dcafc62e8f45a6ad25c1e655a33e18a
Sam Clifford <sj.clifford@gmail.com>
2020-07-16 10:51:12 +0100

    moving plots to a new file

--- script/main.R
+++ script/main.R
@@ -39,42 +39,10 @@ ipd_curves <- ipd_models %>%
   map_df(~mutate(ipd_x, incidence = predict(object = .x, newdata = ipd_x)),
          .id = "serogroup")
 
-#plot fitted curves with backward or forward extrapolation
-incidence_plot <- ggplot(data = ipd,
-                         aes(x = agey,
-                             y = incidence,
-                             color = serogroup)) + 
-  geom_point(size=2.5) + 
-  geom_line(data = ipd_curves) +
-  ylim(c(0, NA)) + 
-  theme_bw() +
-  xlab("Age of vaccination") +
-  ylab("Incidence (cases per 100,000)") +
-  theme(legend.position = "bottom") +
-  scale_color_brewer(palette = "Dark2")
-
-ggsave(here("output","incidence_plot.pdf"),
-       plot = incidence_plot,
-       width = 7, height = 5, unit="in")
-
 #plot scaled incidence
 ipd_scaled <- ipd %>% group_by(serogroup) %>%
   mutate(p = incidence/sum(incidence))
 
-incidence_plot <- ggplot(data = ipd_scaled,
-                         aes(x = agey,
-                             y = p,
-                             color = serogroup)) + 
-  geom_line() +
-  theme_bw() +
-  xlab("Age (years)") +
-  ylab("Scaled incidence") +
-  theme(legend.position = "bottom") +
-  scale_color_brewer(palette = "Dark2")
-
-ggsave(here("output","scaled_plot.pdf"),
-       plot = incidence_plot,
-       width = 7, height = 5, unit="in")
 
 #generate IPD cases from total pop and IPD incidence annually
 # table 7
@@ -181,27 +149,3 @@ impact_by_age_to_plot_max <-
   group_by_at(.vars = vars(-c(Vac.age, Impact))) %>%
   filter(Impact == max(Impact))
 
-impact_by_age_plot <- 
-  ggplot(data = impact_by_age_to_plot,
-         aes(x = Vac.age, y= Impact,
-             color  = factor(age_dep),
-             group = interaction(Waning, age_dep, serogroup, delay,
-                                 Country))) +
-  geom_line() + 
-  facet_grid(Country ~ serogroup + Waning,
-             scales = "free_y") +
-  theme_bw() +
-  scale_y_continuous(limits = c(0,NA)) +
-  xlab("Vaccination Age") +
-  ylab("Impact (expected total cases averted)") +
-  theme(legend.position = "bottom") +
-  scale_color_brewer(name = "Age dependent\nvaccine efficacy",
-                     palette = "Set1") +
-  geom_vline(data = impact_by_age_to_plot_max,
-             aes(xintercept = Vac.age,
-                 color  = factor(age_dep)),
-             lty = 2, alpha = 0.5)
-
-ggsave(filename = "output/impact_by_vac_age.pdf", 
-       plot = impact_by_age_plot,
-       width = 7, height = 4, units = "in")
